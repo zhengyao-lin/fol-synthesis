049d76ed0fed4bb15994c5b05c8c69ce9d4cfc49
rod-lin <zl38@illinois.edu>
2021-07-03 10:37:27 -0500

    Update examples

--- examples/dlist-rev.py
+++ examples/dlist-rev.py
@@ -10,6 +10,7 @@ def synthesize(*args, **kwargs) -> Generator[str, None, None]:
             "list": 1,
             "dlist": 1,
             "dlseg": 2,
+            "rev": 2,
         },
     )
     expanded_language = language.expand(
@@ -21,52 +22,16 @@ def synthesize(*args, **kwargs) -> Generator[str, None, None]:
     )
     synthesizer = HornClauseSynthesizer(env, language, *args, **kwargs)
 
-    ##########################
-    # load concrete examples #
-    ##########################
-    # list1_domain = { 0, 1, 2, 3, 4 }
-    # list1 = FiniteConcreteStructure.create(
-    #     env,
-    #     language,
-    #     list1_domain,
-    #     {
-    #         "nil": 0,
-    #         "next": {
-    #             0: 0,
-    #             1: 0,
-    #             2: 1,
-    #             3: 3,
-    #             4: 3,
-    #         },
-    #         "prev": {
-    #             0: 0,
-    #             1: 2,
-    #             2: 2,
-    #             3: 4,
-    #             4: 4,
-    #         }
-    #     },
-    #     {
-    #         "list": { 0, 1, 2 },
-    #         "dlist": { 0, 1 },
-    #     },
-    # )
-
-    # synthesizer.add_example(list1)
-
-    ################################
-    # construct the counterexample #
-    ################################
-
     apply_nil = lambda structure: structure.eval_function("nil", ())
     apply_n = lambda structure, x: structure.eval_function("n", (x,))
     apply_p = lambda structure, x: structure.eval_function("p", (x,))
     apply_list = lambda structure, x: structure.eval_relation("list", (x,))
     apply_dlist = lambda structure, x: structure.eval_relation("dlist", (x,))
     apply_dlseg = lambda structure, x, y: structure.eval_relation("dlseg", (x, y))
+    apply_rev = lambda structure, x, y: structure.eval_relation("rev", (x, y))
     apply_in_lsegf = lambda structure, x, y, z: structure.eval_relation("in_lsegf", (x, y, z))
     apply_in_lsegb = lambda structure, x, y, z: structure.eval_relation("in_lsegb", (x, y, z))
-    
+
     in_lsegf_defn = lambda structure, x, y, z: \
         If(
             y == z,
@@ -113,6 +78,11 @@ def synthesize(*args, **kwargs) -> Generator[str, None, None]:
         ),
     )
 
+    rev_defn = lambda structure, x, y: Or(
+        z3.And(x == apply_nil(structure), y == apply_nil(structure)),
+        z3.And(x == y, apply_rev(structure, apply_n(structure, x), apply_p(structure, y)))
+    )
+
     syn_counterexample_unrolled = Structure(
         IntSort(),
         expanded_language,
@@ -125,6 +95,7 @@ def synthesize(*args, **kwargs) -> Generator[str, None, None]:
             "list": FreshFunction(IntSort(), BoolSort()),
             "dlist": FreshFunction(IntSort(), BoolSort()),
             "dlseg": FreshFunction(IntSort(), IntSort(), BoolSort()),
+            "rev": FreshFunction(IntSort(), IntSort(), BoolSort()),
             "in_lsegf": FreshFunction(IntSort(), IntSort(), IntSort(), BoolSort()),
             "in_lsegb": FreshFunction(IntSort(), IntSort(), IntSort(), BoolSort()),
         },
@@ -139,6 +110,7 @@ def synthesize(*args, **kwargs) -> Generator[str, None, None]:
                 "list": (lambda structure: lambda x: list_defn(structure, x))(syn_counterexample_unrolled),
                 "dlist": (lambda structure: lambda x: dlist_defn(structure, x))(syn_counterexample_unrolled),
                 "dlseg": (lambda structure: lambda x, y: dlseg_defn(structure, x, y))(syn_counterexample_unrolled),
+                "rev": (lambda structure: lambda x, y: rev_defn(structure, x, y))(syn_counterexample_unrolled),
                 "in_lsegf": (lambda structure: lambda x, y, z: in_lsegf_defn(structure, x, y, z))(syn_counterexample_unrolled),
                 "in_lsegb": (lambda structure: lambda x, y, z: in_lsegb_defn(structure, x, y, z))(syn_counterexample_unrolled),
             },
@@ -170,6 +142,7 @@ def synthesize(*args, **kwargs) -> Generator[str, None, None]:
                 "list": (lambda structure: lambda x: list_defn(structure, x))(ver_counterexample_unrolled),
                 "dlist": (lambda structure: lambda x: dlist_defn(structure, x))(ver_counterexample_unrolled),
                 "dlseg": (lambda structure: lambda x, y: dlseg_defn(structure, x, y))(ver_counterexample_unrolled),
+                "rev": (lambda structure: lambda x, y: rev_defn(structure, x, y))(ver_counterexample_unrolled),
                 "in_lsegf": (lambda structure: lambda x, y, z: in_lsegf_defn(structure, x, y, z))(ver_counterexample_unrolled),
                 "in_lsegb": (lambda structure: lambda x, y, z: in_lsegb_defn(structure, x, y, z))(ver_counterexample_unrolled),
             },
@@ -191,6 +164,12 @@ def synthesize(*args, **kwargs) -> Generator[str, None, None]:
         synthesizer.add_verification_constraint(
             ver_counterexample.eval_relation("dlseg", (x, y)) == \
             dlseg_defn(ver_counterexample_unrolled, x, y),
+        )
+
+    for x, y in ver_counterexample.iterate_universe(2):
+        synthesizer.add_verification_constraint(
+            ver_counterexample.eval_relation("rev", (x, y)) == \
+            rev_defn(ver_counterexample_unrolled, x, y),
         )
 
     for x, y, z in ver_counterexample.iterate_universe(3):
@@ -215,7 +194,7 @@ def main():
         hypothesis_term_depth=0,
         conclusion_term_depth=0,
         max_num_vars=3,
-        max_num_hypotheses=2,
+        max_num_hypotheses=3,
         allow_equality_in_conclusion=False,
     ):
         print(formula)
--- examples/list.py
+++ examples/list.py
@@ -10,8 +10,15 @@ def synthesize(*args, **kwargs) -> Generator[str, None, None]:
             "list": 1,
             "lseg": 2,
             # "len": 2,
-        })
+        },
+    )
+    expanded_language = language.expand(
+        {},
+        { "in_lseg": 3 },
+    )
     synthesizer = HornClauseSynthesizer(env, language, *args, **kwargs)
+
+    synthesizer.dismiss_term(Application.parse("n(nil)"))
 
     ##########################
     # load concrete examples #
@@ -21,103 +28,155 @@ def synthesize(*args, **kwargs) -> Generator[str, None, None]:
     # 4 -> 3 -> 3
     # 5 -> 1
     # list(x) /\ list(y) /\ x != y => n(x) != n(y)
-    list1_domain = { 0, 1, 2, 3, 4 }
-    list1 = FiniteConcreteStructure.create(
-        env,
-        language,
-        list1_domain,
-        {
-            "nil": 0,
-            "n": {
-                0: 0,
-                1: 0,
-                2: 1,
-                3: 3,
-                4: 3,
-            },
-        },
-        {
-            "list": { 0, 1, 2 },
-            "lseg": { (x, y) for x in list1_domain for y in list1_domain if 2 >= x >= y }.union({
-                (3, 3),
-                (4, 3),
-                (4, 4),
-            }),
-            "len": {
-                (0, 0),
-                (1, 1),
-                (2, 2),
-            }
-        },
-    )
+    # list1_domain = { 0, 1, 2, 3, 4 }
+    # list1 = FiniteConcreteStructure.create(
+    #     env,
+    #     language,
+    #     list1_domain,
+    #     {
+    #         "nil": 0,
+    #         "n": {
+    #             0: 0,
+    #             1: 0,
+    #             2: 1,
+    #             3: 3,
+    #             4: 3,
+    #         },
+    #     },
+    #     {
+    #         "list": { 0, 1, 2 },
+    #         "lseg": { (x, y) for x in list1_domain for y in list1_domain if 2 >= x >= y }.union({
+    #             (3, 3),
+    #             (4, 3),
+    #             (4, 4),
+    #         }),
+    #         "len": {
+    #             (0, 0),
+    #             (1, 1),
+    #             (2, 2),
+    #         }
+    #     },
+    # )
 
-    synthesizer.add_example(list1)
+    # synthesizer.add_example(list1)
 
-    ################################
-    # construct the counterexample #
-    ################################
-    nil = 0
-    n = FreshFunction(IntSort(), IntSort())
-    list_unroll0 = FreshFunction(IntSort(), BoolSort())
-    lseg_unroll0 = FreshFunction(IntSort(), IntSort(), BoolSort())
-    in_lseg_unroll0 = FreshFunction(IntSort(), IntSort(), IntSort(), BoolSort())
-    len_unroll0 = FreshFunction(IntSort(), IntSort(), BoolSort())
+    apply_nil = lambda structure: structure.eval_function("nil", ())
+    apply_n = lambda structure, x: structure.eval_function("n", (x,))
+    apply_list = lambda structure, x: structure.eval_relation("list", (x,))
+    apply_lseg = lambda structure, x, y: structure.eval_relation("lseg", (x, y))
+    apply_in_lseg = lambda structure, x, y, z: structure.eval_relation("in_lseg", (x, y, z))
 
-    in_lseg_unroll1 = lambda x, y, z: \
+    in_lseg_defn = lambda structure, x, y, z: \
         If(
             y == z,
             False,
-            Or(x == y, in_lseg_unroll0(x, n(y), z))
+            Or(x == y, apply_in_lseg(structure, x, apply_n(structure, y), z))
         )
 
-    list_unroll1 = lambda x: Or(
-        x == nil,
+    list_defn = lambda structure, x: Or(
+        x == apply_nil(structure),
         And(
-            Not(x == nil),
-            list_unroll0(n(x)),
-            Not(in_lseg_unroll1(x, n(x), nil)),
+            Not(x == apply_nil(structure)),
+            apply_list(structure, apply_n(structure, x)),
+            Not(apply_in_lseg(structure, x, apply_n(structure, x), apply_nil(structure))),
         ),
     )
 
-    lseg_unroll1 = lambda x, y: Or(
+    lseg_defn = lambda structure, x, y: Or(
         x == y,
         And(
             Not(x == y),
-            lseg_unroll0(n(x), y),
-            Not(in_lseg_unroll1(x, n(x), y)),
+            apply_lseg(structure, apply_n(structure, x), y),
+            Not(apply_in_lseg(structure, x, apply_n(structure, x), y)),
         ),
     )
 
-    len_unroll1 = lambda x, y: Or(
-        z3.And(x == nil, y == 0),
-        z3.And(
-            x != nil,
-            y > 0,
-            len_unroll0(n(x), y - 1),
-            Not(in_lseg_unroll1(x, n(x), nil)),
-        ),
-    )
-
-    counterexample = Structure(
+    syn_counterexample_unrolled = Structure(
         IntSort(),
+        expanded_language,
         {
-            "nil": lambda: nil,
-            "n": n,
+            "nil": (lambda nil: lambda: nil)(FreshInt()),
+            "n": FreshFunction(IntSort(), IntSort()),
         },
         {
-            "list": list_unroll1,
-            "lseg": lseg_unroll1,
-            "len": len_unroll1,
+            "list": FreshFunction(IntSort(), BoolSort()),
+            "lseg": FreshFunction(IntSort(), IntSort(), BoolSort()),
+            "in_lseg": FreshFunction(IntSort(), IntSort(), IntSort(), BoolSort()),
         },
     )
 
-    synthesizer.add_counterexample(counterexample, tuple(FreshInt(ctx=env.context) for _ in range(synthesizer.max_num_vars)))
+    for _ in range(2):
+        syn_counterexample_unrolled = Structure(
+            IntSort(),
+            expanded_language,
+            syn_counterexample_unrolled.function_interprets,
+            {
+                "lseg": (lambda structure: lambda x, y: lseg_defn(structure, x, y))(syn_counterexample_unrolled),
+                "list": (lambda structure: lambda x: list_defn(structure, x))(syn_counterexample_unrolled),
+                "in_lseg": (lambda structure: lambda x, y, z: in_lseg_defn(structure, x, y, z))(syn_counterexample_unrolled),
+            },
+        )
+
+    synthesizer.set_synthesis_counterexample(
+        syn_counterexample_unrolled,
+        tuple(FreshInt(ctx=env.context) for _ in range(synthesizer.max_num_vars)),
+    )
+
+    ################# finite counterexample
+    counterexample_size = 5
+
+    ver_counterexample = FiniteSymbolicStructure.create(
+        env,
+        expanded_language,
+        counterexample_size,
+        function_overrides={},
+        relation_overrides={},
+    )
+
+    ver_counterexample_unrolled = ver_counterexample
+    for _ in range(counterexample_size + 1):
+        ver_counterexample_unrolled = Structure(
+            IntSort(),
+            expanded_language,
+            ver_counterexample_unrolled.function_interprets,
+            {
+                "lseg": (lambda structure: lambda x, y: lseg_defn(structure, x, y))(ver_counterexample_unrolled),
+                "list": (lambda structure: lambda x: list_defn(structure, x))(ver_counterexample_unrolled),
+                "in_lseg": (lambda structure: lambda x, y, z: in_lseg_defn(structure, x, y, z))(ver_counterexample_unrolled),
+            },
+        )
+
+    for x, y, z in ver_counterexample.iterate_universe(3):
+        synthesizer.add_verification_constraint(
+            ver_counterexample.eval_relation("in_lseg", (x, y, z)) == \
+            in_lseg_defn(ver_counterexample_unrolled, x, y, z),
+        )
+
+    for x, y in ver_counterexample.iterate_universe(2):
+        synthesizer.add_verification_constraint(
+            ver_counterexample.eval_relation("lseg", (x, y)) == \
+            lseg_defn(ver_counterexample_unrolled, x, y),
+        )
+
+    for x, in ver_counterexample.iterate_universe(1):
+        synthesizer.add_verification_constraint(
+            ver_counterexample.eval_relation("list", (x,)) == \
+            list_defn(ver_counterexample_unrolled, x),
+        )
+
+    synthesizer.set_verification_counterexample(ver_counterexample)
 
     return synthesizer.synthesize()
 
 
 def main():
-    for formula in synthesize(term_depth=1, max_num_vars=3, max_num_hypotheses=2, allow_equality_in_conclusion=True):
+    for formula in synthesize(
+        hypothesis_term_depth=0,
+        conclusion_term_depth=0,
+        max_num_vars=3,
+        max_num_hypotheses=2,
+        allow_equality_in_conclusion=True,
+    ):
         print(formula)
 
 
